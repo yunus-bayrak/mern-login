3687208dc77c75c7ab08edfb7a5b42f5a90bebb2
full-stack-developer124 <62370778+full-stack-developer124@users.noreply.github.com>
2020-07-29 06:09:46 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://johndavis124:)gq07VYe8Di2K1x&m7@cluster0-nhcdz.mongodb.net/mern_auth",
+    mongoURI: "CLOUD_DB_URL/mern_auth",
     // mongoURI: "mongodb://localhost:27017/mern_auth",
     secretOrKey : "secretKey"
 }
